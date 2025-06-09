6aed5e0dd1f25186e6d1d818cab5e33b706c5f9d
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-08 22:04:00 -0400

    Create class_static_non.php

---
+++ class_static_non.php
@@ -0,0 +1,22 @@
+<?php
+
+class A{
+
+public int $a;
+public string $b;
+public static array $c;
+//public $b = "novo";
+function __construct($a = 10, $b = "novo"){
+$this->a = $a;
+$this->b = $b;
+$this::$c = [0 => "Item1", 1 => "Item2", 2 => "Item3"]; 
+}
+} 
+
+$a = new A;
+echo "<pre>";
+echo $a->a; // non-static property
+echo "<br>";
+echo $a->b;
+echo "<br>";
+var_dump($a::$c); // static property
