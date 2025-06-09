719f5c5f69ebfbc42481e325230155daa956b310
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-08 22:12:37 -0400

    Update class_static_non.php

--- class_static_non.php
+++ class_static_non.php
@@ -1,22 +1,25 @@
 <?php
+declare(strict_types=1);
 
-class A{
+/**
+ * Example class with static and non-static properties.
+ */
+class A {
+    public int $a;
+    public string $b;
+    public static array $c = [];
 
-public int $a;
-public string $b;
-public static array $c;
-//public $b = "novo";
-function __construct($a = 10, $b = "novo"){
-$this->a = $a;
-$this->b = $b;
-self::$c = [0 => "Item1", 1 => "Item2", 2 => "Item3"]; 
+    public function __construct(int $a = 10, string $b = "novo") {
+        $this->a = $a;
+        $this->b = $b;
+        self::$c = [0 => "Item1", 1 => "Item2", 2 => "Item3"];
+    }
 }
-} 
 
-$a = new A;
+$a = new A();
 echo "<pre>";
 echo $a->a; // non-static property
 echo "<br>";
 echo $a->b;
 echo "<br>";
-var_dump($a::$c); // static property
+var_dump(A::$c); // static property
